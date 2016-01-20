f4e4790837e33ac0526c0e40903db20781a0a18c
xwang1024 <xwang1024@126.com>
2016-01-20 23:57:02 +0800

    拆分样式文件

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  gulp.src('./*.scss')
+  gulp.src('./scss/index.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./*.scss', ['sass']);
+  gulp.watch('./scss/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass:watch']);
+gulp.task('default', ['sass', 'sass:watch']);
