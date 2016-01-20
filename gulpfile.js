72247a1b5064cbf78472e8998e1de106c238743d
xwang1024 <xwang1024@126.com>
2016-01-21 02:13:57 +0800

    增加样式, 增加图标字体, 增加sprite插件, 侧边栏样式简单写了一下, IE8看了一下还可以

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,31 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var spritesmith = require('gulp.spritesmith');
 
 gulp.task('sass', function () {
   gulp.src('./scss/index.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./css/'));
+  gulp.src('./scss/icon/ionicons.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./scss/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('./images/sprite/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css'
+  }));
+  return spriteData.pipe(gulp.dest('./sprite/'));
+});
+
+gulp.task('sprite:watch', function () {
+  gulp.watch('./images/sprite/*.png', ['sprite']);
+});
+
+gulp.task('default', ['sass', 'sprite', 'sass:watch', 'sprite:watch']);
