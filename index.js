8d9de136c7fbb4cbd22abf7cec6dc3122a209045
xwang1024 <xwang1024@126.com>
2016-01-26 19:48:20 +0800

    layout可以变形了

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $('.nav-title').bind('click', function() {
     $(this).siblings('.nav-toggle-menu').slideToggle(200).toggleClass('active');
   });
+  $('.layout-transform-btn').bind('click', function() {
+    $('body').toggleClass('transform');
+  });
 });
