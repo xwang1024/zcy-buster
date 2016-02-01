71aa5cddae0649080b7e62731705b431df6ab118
xwang1024 <xwang1024@126.com>
2016-02-01 21:45:09 +0800

    box-sizing ok, 收起状态下的右侧展开菜单有眉目了

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ $(document).ready(function() {
   });
   $('.layout-transform-btn').bind('click', function() {
     $('body').toggleClass('transform');
+    $('.nav-title').unbind();
+    if($('body').hasClass('transform')) {
+      ;
+    } else {
+      $('.nav-title').bind('click', function() {
+        $(this).siblings('.nav-toggle-menu').slideToggle(200).toggleClass('active');
+      });
+    }
   });
 });
