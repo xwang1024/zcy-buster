f5d20cac3050ec42e04353c0f0da49f90547dc00
xwang1024 <xwang1024@126.com>
2016-02-02 21:28:24 +0800

    侧边栏ok

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 $(document).ready(function() {
-  $('.nav-title').bind('click', function() {
-    $(this).siblings('.nav-toggle-menu').slideToggle(200).toggleClass('active');
-  });
-  $('.layout-transform-btn').bind('click', function() {
-    $('body').toggleClass('transform');
-    $('.nav-title').unbind();
-    if($('body').hasClass('transform')) {
-      ;
+  // 当前是否处于收起状态
+  var isCollapsed = $('body').hasClass('collapsed');
+  var initSidebar = function() {
+    // 针对不同的情况初始化不同的事件
+    if(isCollapsed) {
+      $('.nav-title').unbind().bind('mouseover click', function(event) {
+        event.stopPropagation();
+        $('.nav-toggle-menu.nav-menu-float').trigger('mouseleave');
+        $(this).siblings('.nav-toggle-menu').clone()
+          .appendTo($(this).parents('.sidebar'))
+          .addClass('nav-menu-float')
+          .css({"top": parseInt($(this).offset().top), "display": "block"})
+          .bind('mouseleave', function(event) {
+            $('.nav-toggle-menu.nav-menu-float').remove();
+          })
+          .bind('mouseover', function(event) {
+            event.stopPropagation();
+          });
+
+
+      });
+      $('body').unbind().bind('mouseover click', function(event) {
+        $('.nav-toggle-menu.nav-menu-float').trigger('mouseleave');
+      })
     } else {
-      $('.nav-title').bind('click', function() {
+      // 绑定父级菜单的点击事件
+      $('.nav-title').unbind().bind('click', function() {
         $(this).siblings('.nav-toggle-menu').slideToggle(200).toggleClass('active');
       });
     }
+  }
+
+  // 绑定底部变形按钮
+  $('.layout-collapse-btn').bind('click', function() {
+    $('body').toggleClass('collapsed');
+    isCollapsed = !isCollapsed;
+    initSidebar();
   });
+
+  initSidebar();
+  
 });
